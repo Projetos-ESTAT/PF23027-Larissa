f3dfd320c83aa2491b9a0345c2f441cf2cb6b333
StefanZur <stefanzurmangoncalves@gmail.com>
2023-08-18 11:55:49 -0300

    dor e sofrimento

--- rdocs/Stefan.R
+++ rdocs/Stefan.R
@@ -2,8 +2,20 @@ source("rdocs/source/packages.R")
 pacman::p_load(
   "readxl", "dplyr", "ggplot2", "tidyr",
   "kableExtra", "ggcorrplot", "psych", "purrr",
-  "caret", "gvlma", "lmtest"
+  "caret", "gvlma", "lmtest", "MASS"
 )
+library(summarytools)
+library(olsrr)
+require(lmtest)
+require(lawstat)
+library(EnvStats)
+library(car)
+require(leaps)
+library(caret) 
+library(readxl)
+library(Hmisc)
+library(corrplot)
+library(tidyverse)
 
 # ---------------------------------------------------------------------------- #
 
@@ -50,15 +62,43 @@ View(banco[banco$CAD + banco$PMP - banco$CC>1,])
 
 reg3 <- lm(data = banco2, CAD ~ ALTITUDE + PROFUND + DENSIDADE + AREIA_GROS + AREIA_FINA + SILTE + ARGILA)
 
+
+
 summary(reg3)
 
 plot(reg3$fitted.values,banco2$CAD)
 
-reg4 <- lm(data = banco2, CAD ~ ALTITUDE + PROFUND + DENSIDADE)
+plot(reg3$fitted.values,reg3$residuals)
+
+
+reg4 <- lm(data = banco2, CAD ~ ALTITUDE + PROFUND)
 
 summary(reg4)
 
 plot(reg4$fitted.values,banco2$CAD)
+
+plot(reg4$fitted.values,reg4$residuals)
+
+# estimating the variance of y for different values of x
+variance = lm(abs(reg4$residuals) ~ reg4$fitted.values)$fitted.values^2
+# calculating the weights
+weights = (1 / variance)
+# weighted regression model
+weighted_model = reg3 <- lm(data = banco2, CAD ~ ALTITUDE + PROFUND + DENSIDADE + AREIA_GROS  + SILTE + ARGILA, weights = weights)
+summary(weighted_model)
+plot(weighted_model$fitted.values,regStep$residuals)
+plot(weighted_model$fitted.values,banco2$CAD)
+bptest(weighted_model)
+
+
+
+banco1<-banco%>%
+  dplyr::select(CAD, Altitude = ALTITUDE, `Areia Fina`= AREIA_FINA,`Areia Grossa`=AREIA_GROS, Argila=ARGILA,  Densidade=DENSIDADE, Profundidade = PROFUND,   Silte=SILTE)
+corr_matrix = round(cor(banco1), 2)
+ggcorrplot(corr_matrix, hc.order = TRUE, type = "lower",
+           lab = TRUE)
+
+
 
 
 regStep <- step(reg3)
@@ -66,3 +106,52 @@ regStep <- step(reg3)
 summary(regStep)
 
 plot(regStep$fitted.values,banco2$CAD)
+
+plot(regStep$fitted.values,regStep$residuals)
+
+# estimating the variance of y for different values of x
+variance = lm(abs(regStep$residuals) ~ regStep$fitted.values)$fitted.values^2
+# calculating the weights
+weights = ((1 / variance))
+# weighted regression model
+weighted_model = reg3 <- lm(data = banco2, CAD ~ ALTITUDE + PROFUND + DENSIDADE + AREIA_GROS  + SILTE + ARGILA, weights = weights)
+summary(weighted_model)
+plot(weighted_model$fitted.values,regStep$residuals)
+plot(weighted_model$fitted.values,banco2$CAD)
+bptest(weighted_model)
+
+
+
+
+
+
+
+
+
+
+
+
+
+box <- boxcox(data = banco2, CAD ~ log(ALTITUDE) + log(PROFUND) + log(DENSIDADE) + log(AREIA_GROS) + log(AREIA_FINA) + log(SILTE) + log(ARGILA))
+
+lambda <- box$x[which.max(box$y)]
+
+reg32 <- lm(data = banco2, (CAD ^ lambda - 1) / lambda ~ log(ALTITUDE) + log(PROFUND) + log(DENSIDADE) + log(AREIA_GROS) + log(AREIA_FINA) + log(SILTE) + log(ARGILA))
+
+plot(reg32$fitted.values,banco2$CAD)
+
+plot(reg32$fitted.values,reg32$residuals)
+
+
+regStep2 <- step(reg32)
+
+summary(regStep2)
+
+plot(regStep2$fitted.values,banco2$CAD)
+
+plot(regStep2$fitted.values,regStep2$residuals)
+
+shapiro.test(reg32$residuals)
+qqnorm(reg32$residuals)
+qqline(reg32$residuals)
+bptest(reg32)
